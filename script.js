925f4af98ef86dd8c10a4fa5c5e231e34376101d
Maria <pascoe_maria@hotmail.com>
2022-03-19 12:41:31 +0000

    hitRandimShip function working

--- script.js
+++ script.js
@@ -23,12 +23,13 @@ class Ship {
   }
 
   remainingPoints() {
-    return this.startPoints - this.hitPoints;
+    return (this.startPoints = this.startPoints - this.hitPoints);
   }
 
   renderShip() {
-    return `<div class='ship ${this.id}' data-id=${this.id}>
+    return `<div class='ship' data-id=${this.id}>
       <h3>${this.name}</h3>
+      <p>${this.id}</p>
       <p class='startPoints'>${this.startPoints}</p>
     </div>`;
   }
@@ -57,57 +58,104 @@ for (let i = 0; i < 8; i++) {
     attackShip.renderShip();
   console.log(attackShip);
 }
-console.log(shipsArr);
 
+console.log(shipsArr);
+console.log(shipsArr[0].name); //mother ship
+
+const allShipsHTML = document.querySelectorAll(".ship");
+console.log(allShipsHTML);
+
+const getRandomShip = () => {
+  return Math.floor(Math.random() * allShipsHTML.length);
+};
+getRandomShip();
+
+// console.log(getRandomShip()); // 7
+// console.log(allShipsHTML[getRandomShip()].children[1].innerHTML); // S-2
+// console.log(allShipsHTML[getRandomShip()]); // div with id S-8
+
+// const randomShipIndex = getRandomShip();
+// console.log(randomShipIndex);
+// console.log(allShipsHTML[randomShipIndex].children[1].innerHTML);
+// console.log(allShipsHTML[randomShipIndex]);
+
+//let remainingPoints = shipsArr[].remainingPoints()
+//
 const hitRandomShip = () => {
   button.addEventListener("click", () => {
-    const randomShipIndex = Math.floor(Math.random() * shipsArr.length);
-    const randomShipId = shipsArr[randomShipIndex].id;
-    // console.log(randomShipIndex.querySelector(randomShipId));
-    console.log(`Ship ID is: ${randomShipId}`);
-    // const shipNode = document.querySelector()
-    // console.log(randomShipIndex);
-    // console.log(shipsArr[randomShipIndex].startPoints);
-    // console.log(shipsArr[randomShipIndex].hitPoints);
-    // console.log(shipsArr[randomShipIndex].id);
+    const randomShipIndex = getRandomShip();
+    console.log(randomShipIndex);
+    const currentShip = shipsArr[randomShipIndex];
+    console.log(currentShip);
 
-    let remainingPoints = shipsArr[randomShipIndex].remainingPoints();
-    console.log(
-      `Ship Index: ${shipsArr[randomShipIndex].id}, Points remainining: ${remainingPoints}`
-    );
+    const hitShipRemainingPoints = currentShip.remainingPoints();
+    console.log(hitShipRemainingPoints);
 
-    // let remainingPoints =
-    //   shipsArr[randomShipIndex].startPoints -
-    //   shipsArr[randomShipIndex].hitPoints;
+    // console.log(allShipsHTML[randomShipIndex]);
+    // console.log(allShipsHTML[randomShipIndex].children[0].innerHTML);
+    // console.log(allShipsHTML[randomShipIndex].children[1].innerHTML);
+    // console.log(allShipsHTML[randomShipIndex].children[2].innerHTML);
 
-    // const motherShip = shipsArr[0].id === "S-1";
-
-    // if (motherShip === true && remainingPoints <= 0) {
-    //   document.querySelector("#allShipsContainer").innerHTML = "GAME OVER";
-    // } else
-    if (remainingPoints <= 30) {
-      shipsArr.splice(randomShipIndex, 1);
-
-      // document.querySelector("#allShipsContainer")[randomShipIndex].innerHTML =
-      //   "";
-      console.log(shipsArr);
-    } else {
-      shipsArr[randomShipIndex].startPoints = remainingPoints;
-      // console.log(shipsArr[randomShipIndex].startPoints);
-    }
-
-    // document.querySelector(".startPoints").innerHTML = remainingPoints;
+    allShipsHTML[randomShipIndex].children[2].innerHTML =
+      hitShipRemainingPoints;
   });
 };
 hitRandomShip();
 
-// const currentShipPoints = document.querySelector(".startPoints").innerHTML;
-// console.log(currentShipPoints);
+// const hitRandomShip = () => {
+//   button.addEventListener("click", () => {
+//     const randomShipIndex = Math.floor(Math.random() * shipsArr.length);
+//     const randomShipId = shipsArr[randomShipIndex].id;
+//     // console.log(randomShipIndex.querySelector(randomShipId));
+//     console.log(`Ship ID is: ${randomShipId}`);
 
-// const allShipsArr = allShips.childNodes;
-// // allShipsArr.push(motherShip, defenceShip);
-// console.log(allShipsArr);
+//     //const currentShipHTML = document.body.dataset.id === randomShipId;
+//     console.log(randomShipIndex);
 
-// class MotherShip {
-//   // MOTHER SHIP DESTROYED = ALL SHIPS DESTROYED -> GAME OVER
-// }
+//     // const shipNode = document.querySelector()
+//     // console.log(randomShipIndex);
+//     // console.log(shipsArr[randomShipIndex].startPoints);
+//     // console.log(shipsArr[randomShipIndex].hitPoints);
+//     //console.log(shipsArr[randomShipIndex]);
+
+//     let remainingPoints = shipsArr[randomShipIndex].remainingPoints();
+//     console.log(
+//       `Ship Index: ${shipsArr[randomShipIndex].id}, Points remainining: ${remainingPoints}`
+//     );
+
+//     // let remainingPoints =
+//     //   shipsArr[randomShipIndex].startPoints -
+//     //   shipsArr[randomShipIndex].hitPoints;
+
+//     // const motherShip = shipsArr[0].id === "S-1";
+
+//     // if (motherShip === true && remainingPoints <= 0) {
+//     //   document.querySelector("#allShipsContainer").innerHTML = "GAME OVER";
+//     // } else
+//     if (remainingPoints <= 30) {
+//       shipsArr.splice(randomShipIndex, 1);
+
+//       // document.querySelector("#allShipsContainer")[randomShipIndex].innerHTML =
+//       //   "";
+
+//       console.log(shipsArr);
+//     } else {
+//       shipsArr[randomShipIndex].startPoints = remainingPoints;
+//       // console.log(shipsArr[randomShipIndex].startPoints);
+//     }
+
+//     // document.querySelector(".startPoints").innerHTML = remainingPoints;
+//   });
+// };
+// hitRandomShip();
+
+// // const currentShipPoints = document.querySelector(".startPoints").innerHTML;
+// // console.log(currentShipPoints);
+
+// // const allShipsArr = allShips.childNodes;
+// // // allShipsArr.push(motherShip, defenceShip);
+// // console.log(allShipsArr);
+
+// // class MotherShip {
+// //   // MOTHER SHIP DESTROYED = ALL SHIPS DESTROYED -> GAME OVER
+// // }
